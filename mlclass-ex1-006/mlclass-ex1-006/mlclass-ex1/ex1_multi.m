093292fd88d05ba1cdfde9bb7387d1119297c61a
chenrudan <chenrudan123@gmail.com>
2014-07-11 20:58:25 +0800

    work2

--- mlclass-ex1-006/mlclass-ex1-006/mlclass-ex1/ex1_multi.m
+++ mlclass-ex1-006/mlclass-ex1-006/mlclass-ex1/ex1_multi.m
@@ -84,8 +84,6 @@ fprintf('Running gradient descent ...\n');
 % Choose some alpha value
 alpha = 0.01;
 num_iters = 400;
-%alpha = 0.1;
-%num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -107,7 +105,7 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-price = [ones(1, 1), ([1650,3] - mu)./sigma]*theta;
+
 
 % ============================================================
 
@@ -152,7 +150,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-price = [ones(1, 1), [1650,3]]*theta;
+
 
 % ============================================================
 
--- mlclass-ex1-006/mlclass-ex1-006/mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1-006/mlclass-ex1-006/mlclass-ex1/gradientDescentMulti.m
@@ -16,7 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-	theta = theta - (alpha/m)*((theta'*X' - y')*X)';
+
+
+
+
+
+
+
+
+
+
+
     % ============================================================
 
     % Save the cost J in every iteration    
